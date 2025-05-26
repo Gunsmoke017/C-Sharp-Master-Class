1d1f3e539aaa2eab407a163ec93a6119a16a0fb2
Kien Olali Kinggs <Percivalrp106@gmail.com>
2025-05-26 20:33:55 +0100

    extra stuff

---
+++ MyAdditionClaculator/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter the first numer:");
+
+int first = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second number:");
+int second = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("The sum of the two numbers is: " + (first + second));
