abcebb27767d7ae521d83c51fdba4117b22f32e0
Kien Olali Kinggs <Percivalrp106@gmail.com>
2025-06-11 11:03:26 +0100

    added test console calculator from course project

--- MyAdditionClaculator/Program.cs
+++ MyAdditionClaculator/Program.cs
@@ -5,3 +5,44 @@ int first = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter the second number:");
 int second = Convert.ToInt32(Console.ReadLine());
  Console.WriteLine("The sum of the two numbers is: " + (first + second));
+
+
+            int num1;
+            int num2;
+            string operation;
+            
+            Console.WriteLine($"Enter the first number:");
+            int.TryParse(Console.ReadLine(), out num1);
+            
+            Console.WriteLine($"Enter the second number:");
+            int.TryParse(Console.ReadLine(), out num2);
+            
+            Console.WriteLine($"Choose an operation: +,-,*,/");
+            operation = Console.ReadLine();
+            
+            switch(operation)
+            {
+                case "+":
+                    Console.WriteLine($"Result : {num1 + num2}");
+                    break;
+                case "-":
+                    Console.WriteLine($"Result : {num1 - num2}");
+                    break;
+                case "*":
+                    Console.WriteLine($"Result : {num1 / num2}");
+                    break;
+                case "/":
+                    if(num2 == 0) 
+                    {
+                        Console.WriteLine("Error: Division by zero is not allowed");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Result : {num1 / num2}");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation. \n Please choose +,-,* or /.");
+                    break;
+            }
+            
